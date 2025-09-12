411e75d69387a65ff9e5c211e92cbf98368d657d
AlbertoRC22 <AlbertoRC@uma.es>
2025-09-12 16:36:50 +0200

    Commit final

--- 1.1. alice_bob sin clave/.ipynb_checkpoints/data_utils-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-# Se generan n mensajes binarios aleatoriamente de los bits especificados
-def generar_mensajes(n, bits):
-    return np.random.randint(0, 2, size=(n, bits)).astype(np.float32)
